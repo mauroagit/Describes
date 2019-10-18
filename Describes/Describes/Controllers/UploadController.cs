d52676ce292e7196bb7af1ef9359a3d7b694cb53
William Ortiz <waortiz@gmail.com>
2019-10-18 16:59:34 -0500

    New API

---
+++ Describes/Describes/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Describes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public UploadController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Post(FileModel file)
+        {
+            var uploads = configuration.GetValue<string>("AppConfiguration:Uploads");
+            var bytes = Convert.FromBase64String(file.File);
+            using (ZipArchive archive = new ZipArchive(new MemoryStream(bytes)))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (!string.IsNullOrEmpty(Path.GetExtension(entry.FullName))) //make sure it's not a folder
+                    {
+                        entry.ExtractToFile(Path.Combine(uploads, entry.FullName));
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(Path.Combine(uploads, entry.FullName));
+                    }
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Describes/Describes/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Describes
+{
+    public class FileModel
+    {
+        public string Id { get; set; }
+        public string File { get; set; }
+    }
+}
